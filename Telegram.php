236380ba4a154f69ef938f7c6f6e520019728fd1
xechrome <yanruziqa@gmail.com>
2021-01-25 08:58:16 +0700

    Today idea

--- Telegram.php
+++ Telegram.php
@@ -3,8 +3,20 @@
     $update = json_decode(file_get_contents("php://input"), TRUE);
 
     $chatId = $update["message"]["chat"]["id"];
-    if($chatId == NULL)
-        $chatId = "935108127";
     $message = $update["message"]["text"];
-    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Ini dari bot kita");
+
+    $db = new SQLite3('data.db');
+    $ideData = $db->exec('SELECT * FROM idea');
+    $ide_1 = rand(0, count($ideData)-1);
+    $firstIdea = $ideData[$ide_1];
+
+    if(strpos($message, "/today") === 0){
+        file_get_contents(
+            $path.
+            "/sendmessage?chat_id=".
+            $chatId.
+            "&text=".
+            $firstIdea['content']
+        );
+    }
 ?>
